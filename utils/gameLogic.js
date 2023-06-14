edaee1a4974dd745200d051b3ffc1ff41864edd9
bsfelizardo <bsfelizardo@up.edu.ph>
2023-06-15 04:33:04 +0800

    start on game logic

---
+++ utils/gameLogic.js
@@ -0,0 +1,11 @@
+var raft_properties = {
+    passenger_1: 0,
+    passenger_2: 0,
+}
+
+
+const listen = () => {
+    window.onkeyup = (key) => {
+        console.log(`Key: ${key.key}, Code: ${key.code}`)
+    }
+}
