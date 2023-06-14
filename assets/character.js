2ddde575ad3d2c4c312e3caecc7a2540fbb99b64
seancua <71418643+seancua@users.noreply.github.com>
2023-06-15 02:24:33 +0800

    scale: character

--- assets/character.js
+++ assets/character.js
@@ -75,5 +75,50 @@ for (let i = 0; i < char_head_righteye.length; i+=4){
 }
 
 for (let i = 2; i < char_head_righteye.length; i+=4){
-  char_head_righteye[i] = -0.2
+  char_head_righteye[i] = -0.1
+  char_head_lefteye[i] = -0.1
 }
+
+// scale
+for (let i = 0; i < char_head_front.length; i+=4){
+  char_head_front[i] = char_head_front[i] * 0.5
+  char_head_front[i+1] = char_head_front[i+1] * 0.5
+  char_head_front[i+2] = 0.2
+}
+
+for (let i = 0; i < char_head_back.length; i+=4){
+  char_head_back[i] = char_head_back[i] * 0.5
+  char_head_back[i+1] = char_head_back[i+1] * 0.5
+  char_head_back[i+2] = 0.1
+}
+
+for (let i = 0; i < char_head_left.length; i+=4){
+  char_head_left[i] = char_head_left[i] * 0.5
+  char_head_left[i+1] = char_head_left[i+1] * 0.5
+  char_head_left[i+2] = char_head_left[i+2] > 0 ? 0.2 : 0.1
+}
+
+for (let i = 0; i < char_head_right.length; i+=4){
+  char_head_right[i] = char_head_right[i] * 0.5
+  char_head_right[i+1] = char_head_right[i+1] * 0.5
+  char_head_right[i+2] = char_head_right[i+2] > 0 ? 0.2 : 0.1
+}
+
+for (let i = 0; i < char_head_top.length; i+=4){
+  char_head_top[i] = char_head_top[i] * 0.5
+  char_head_top[i+1] = char_head_top[i+1] * 0.5
+  char_head_top[i+2] = char_head_top[i+2] > 0 ? 0.2 : 0.1
+}
+
+for (let i = 0; i < char_head_lefteye.length; i+=4){
+  char_head_lefteye[i] = char_head_lefteye[i] * 0.5
+  char_head_lefteye[i+1] = char_head_lefteye[i+1] * 0.5
+  char_head_lefteye[i+2] = 0.1
+}
+
+for (let i = 0; i < char_head_righteye.length; i+=4){
+  char_head_righteye[i] = char_head_righteye[i] * 0.5
+  char_head_righteye[i+1] = char_head_righteye[i+1] * 0.5
+  char_head_righteye[i+2] = 0.1
+}
+
